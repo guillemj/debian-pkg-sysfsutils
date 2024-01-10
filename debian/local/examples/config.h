37c1d35a6df8f7999c885dd5d735f2ddf7eb8822
Guillem Jover <guillem@debian.org>
2024-01-11 00:04:56 +0100

    Improve examples scaffolding
    - Provide a stub config.h with constant contents needed by the examples.
    - Provide a VPATH-enabled Makefile to build the examples.
    - Rename README.Examples to README.
    - Rewrite the README.

---
+++ debian/local/examples/config.h
@@ -0,0 +1,20 @@
+/*
+ * Config header for libsysfs example programs.
+ */
+
+#ifndef SYSFS_CONFIG_H
+#define SYSFS_CONFIG_H 1
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE 1
+#endif
+
+#ifndef _POSIX_SOURCE
+#define _POSIX_SOURCE 1
+#endif
+
+#ifndef _FILE_OFFSET_BITS
+#define _FILE_OFFSET_BITS 64
+#endif
+
+#endif
